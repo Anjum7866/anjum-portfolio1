b39176ce2097e04465b3ed405fdb91c9ba2f693d
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-21 10:55:49 -0700

    test

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,31 +1,31 @@
- import React  from "react";
- import { Route, Routes } from "react-router-dom";
- import Navbar from "../components/Navbar";
-// import {ThemeProvider, createGlobalStyle} from 'styled-components';
+import React ,{useState} from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import {ThemeProvider, createGlobalStyle} from 'styled-components';
 
-// import '../App.css'
+import '../App.css'
 
-// const GlobalStyle = createGlobalStyle`
-//   body{
-//   background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
-//   }
+const GlobalStyle = createGlobalStyle`
+  body{
+  background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
+  }
 
-// `
+`
 
 const Layout = () => {
-//   const [theme, setTheme] = useState({ mode: 'light'});
-   return (
-//     <ThemeProvider theme={theme}>
+  const [theme, setTheme] = useState({ mode: 'light'});
+  return (
+    <ThemeProvider theme={theme}>
     <>
-   {/* <GlobalStyle /> */}
+  <GlobalStyle />
    <Routes>
             <Route path="/" element={<Navbar/>}/>
             {/* <Route path="/iPrint" element={<Iprint/>}/> */}
     </Routes>
 
     </>
-//     </ThemeProvider>
-   );
+    </ThemeProvider>
+  );
 };
 
 export default Layout;
