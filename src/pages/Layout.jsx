99d8da099650d392de48db2ec452e372f4406ed3
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-21 10:33:28 -0700

    testing on netlify

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,30 +1,30 @@
 import React ,{useState} from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
-import {ThemeProvider, createGlobalStyle} from 'styled-components';
+// import {ThemeProvider, createGlobalStyle} from 'styled-components';
 
 import '../App.css'
 
-const GlobalStyle = createGlobalStyle`
-  body{
-  background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
-  }
+// const GlobalStyle = createGlobalStyle`
+//   body{
+//   background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
+//   }
 
-`
+// `
 
 const Layout = () => {
-  const [theme, setTheme] = useState({ mode: 'light'});
+  // const [theme, setTheme] = useState({ mode: 'light'});
   return (
-    <ThemeProvider theme={theme}>
+    // <ThemeProvider theme={theme}>
     <>
-  <GlobalStyle />
+  {/* <GlobalStyle /> */}
    <Routes>
             <Route path="/" element={<Navbar/>}/>
             {/* <Route path="/iPrint" element={<Iprint/>}/> */}
     </Routes>
 
     </>
-    </ThemeProvider>
+    // </ThemeProvider>
   );
 };
 
