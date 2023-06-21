7b5dff9c3549faeb611e49cd1f9aba64c3dd1dce
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-21 11:26:08 -0700

    test on netlify

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,28 +1,28 @@
-import React  from "react";
+import React,{useState}  from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Home from "../contents/Home";
-// import About from "../contents/About";
-// import Contact from "../contents/Contact";
-// import Portfolio from "../contents/Portfolio";
+import About from "../contents/About";
+import Contact from "../contents/Contact";
+import Portfolio from "../contents/Portfolio";
 
-// import {ThemeProvider, createGlobalStyle} from 'styled-components';
+ import {ThemeProvider, createGlobalStyle} from 'styled-components';
 
 import '../App.css'
 
-// const GlobalStyle = createGlobalStyle`
-//   body{
-//   background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
-//   }
+const GlobalStyle = createGlobalStyle`
+  body{
+  background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
+  }
 
-// `
+`
 
 const Layout = () => {
-  // const [theme, setTheme] = useState({ mode: 'light'});
+  const [theme, setTheme] = useState({ mode: 'light'});
   return (
-    // <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
     <>
-  {/* <GlobalStyle /> */}
+  <GlobalStyle />
   <div className="App">
     <Navbar/>
    <Routes>
@@ -30,19 +30,20 @@ const Layout = () => {
         
     
     <Route path="/" element={<Home/>}/>
-    {/* <Route path="/about" element={<About/>}/>
+    <Route path="/about" element={<About/>}/>
     <Route path="/contact" element={<Contact/>}/>
-    <Route path="/portfolio" element={<Portfolio/>}/> */}
+    <Route path="/portfolio" element={<Portfolio/>}/>
     
-    {/* <div class="back1 glass" onClick={e => setTheme(
-      theme.mode === 'dark' ? {mode: 'light'} : {mode: 'dark'}
-    )}><i class={theme.mode === 'dark' ? "fa fa-sun i1" : "fa fa-moon i1"}></i></div>
-    */}
+    
     
     </Routes>
+    <div class="back1 glass" onClick={e => setTheme(
+      theme.mode === 'dark' ? {mode: 'light'} : {mode: 'dark'}
+    )}><i class={theme.mode === 'dark' ? "fa fa-sun i1" : "fa fa-moon i1"}></i></div>
+   
     </div>
     </>
-    // </ThemeProvider>
+     </ThemeProvider>
   );
 };
 
