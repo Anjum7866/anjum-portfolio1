7e7fc4b8f154cdca54c136a7bd40d218e254df99
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-21 12:27:41 -0700

    working fine

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 class Social extends Component {
     render() {
         return (
+            
             <div class="social glass">
+               
             <div class="btnsame btn1">
-				<a href="https://github.com/anjum7866"><i class="fa fa-github"></i></a>
+				<a href="https://github.com/anjum7866"><FontAwesomeIcon icon={faGithub} className="i1" size='2x'/></a>
 			</div>
 			<div class="btnsame btn3">
-				<a href="https://linkedin.com/in/anjum-shaikh-2757a020a" ><i class="fa fa-linkedin"></i></a>
+				<a href="https://linkedin.com/in/anjum-shaikh-2757a020a" ><FontAwesomeIcon icon={faLinkedin} className="i1" size='2x'/></a>
 			</div>
 			<div class="btnsame btn4">
-				<a href="https://instagram.com/fitcoach_anjumshaikh" ><i class="fa fa-instagram"></i></a>
+				<a href="https://instagram.com/fitcoach_anjumshaikh" ><FontAwesomeIcon icon={faInstagram} className="i1" size='2x'/></a>
 			</div>
             </div>
             )
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -5,6 +5,7 @@ import Home from "../contents/Home";
 import About from "../contents/About";
 import Contact from "../contents/Contact";
 import Portfolio from "../contents/Portfolio";
+import { FaSun, FaMoon } from 'react-icons/fa';
 
  import {ThemeProvider, createGlobalStyle} from 'styled-components';
 
@@ -12,11 +13,10 @@ import '../App.css'
 
 const GlobalStyle = createGlobalStyle`
   body{
-  background: linear-gradient(${props => props.theme.mode === 'dark' ? '#868f96, #596164' : '(#ff758c,#764ba2'});
+  background: linear-gradient(${props => props.theme.mode === 'dark' ? '#923CB5, #000000' : '(#ff758c,#764ba2'});
   }
 
 `
-
 const Layout = () => {
   const [theme, setTheme] = useState({ mode: 'light'});
   return (
@@ -34,14 +34,14 @@ const Layout = () => {
     <Route path="/contact" element={<Contact/>}/>
     <Route path="/portfolio" element={<Portfolio/>}/>
     
-    
-    
+   
     </Routes>
     <div class="back1 glass" onClick={e => setTheme(
       theme.mode === 'dark' ? {mode: 'light'} : {mode: 'dark'}
-    )}><i class={theme.mode === 'dark' ? "fa fa-sun i1" : "fa fa-moon i1"}></i></div>
-   
+    )}>{theme.mode === 'dark' ?(<FaSun className="i1"/>)  :(<FaMoon className="i1" />) }</div>
+  
     </div>
+    
     </>
      </ThemeProvider>
   );
